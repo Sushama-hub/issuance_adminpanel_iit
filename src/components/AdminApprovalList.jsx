c19253b656c389f3ac7c41daa70d95d58291783e
D-Sushma <sushama@iitbhilai.ac.in>
2025-04-28 17:55:03 +0530

    Reapply "added cancel button functinality"
    This reverts commit 0e7b3e0bfa132f44ff8b4f3f4888d8fe6fbd62c2.

--- src/components/AdminApprovalList.jsx
+++ src/components/AdminApprovalList.jsx
@@ -36,10 +36,15 @@ export default function AdminApproval() {
 
   const handleApproveUser = async (userId) => {
     console.log("handleApproveUser called", userId)
-    const response = await axios.put(`${baseURL}/admin/approve/${userId}`)
-    console.log("response approve====", response.data)
+    await axios.put(`${baseURL}/admin/approve/${userId}`)
     alert("User approved as admin")
-    // fetchUsers()
+    window.location.reload()
+  }
+
+  const handleRemoveUser = async (userId) => {
+    console.log("handleApproveUser called", userId)
+    alert("Do You Want To Denied User!")
+    await axios.delete(`${baseURL}/admin/deletePendingUser/${userId}`)
     window.location.reload()
   }
 
@@ -101,15 +106,26 @@ export default function AdminApproval() {
                       {user.email}
                     </Typography>
                   </Box>
-                  <Button
-                    variant="outlined"
-                    color="success"
-                    size="small"
-                    onClick={() => handleApproveUser(user?._id)}
-                    sx={{ whiteSpace: "nowrap", mt: 2 }} //prevents line break inside button
-                  >
-                    Approve as Admin
-                  </Button>
+                  <Box sx={{ display: "flex", gap: 1 }}>
+                    <Button
+                      variant="outlined"
+                      color="success"
+                      size="small"
+                      onClick={() => handleApproveUser(user?._id)}
+                      sx={{ whiteSpace: "nowrap", mt: 2 }} //prevents line break inside button
+                    >
+                      Approve as Admin
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      color="error"
+                      size="small"
+                      onClick={() => handleRemoveUser(user?._id)}
+                      sx={{ whiteSpace: "nowrap", mt: 2 }} //prevents line break inside button
+                    >
+                      cancel
+                    </Button>
+                  </Box>
                 </CardContent>
               </Card>
             ))}
